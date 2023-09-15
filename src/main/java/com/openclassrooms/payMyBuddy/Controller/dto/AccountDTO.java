883c35eb0885f25ee88778e9adc777f2fbe50520
joel Dumortier <joel.dumortier@gmail.com>
2023-09-15 16:17:43 +0200

    create accountDTO

---
+++ src/main/java/com/openclassrooms/payMyBuddy/Controller/dto/AccountDTO.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.payMyBuddy.Controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountDTO {
+
+    private BigDecimal balance;
+
+}
