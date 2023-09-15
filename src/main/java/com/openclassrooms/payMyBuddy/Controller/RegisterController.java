20440091eb9835445abe0910b2791ad248adf15a
joel Dumortier <joel.dumortier@gmail.com>
2023-09-15 16:43:39 +0200

    create register controller and register page

---
+++ src/main/java/com/openclassrooms/payMyBuddy/Controller/RegisterController.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.payMyBuddy.Controller;
+
+import com.openclassrooms.payMyBuddy.Controller.dto.UserDTO;
+import com.openclassrooms.payMyBuddy.exceptions.UserAlreadyExistException;
+import com.openclassrooms.payMyBuddy.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return ("register");
+    }
+
+    @PostMapping("/register")
+    public String submitRegisterForm(@Valid @ModelAttribute("user") UserDTO user, Errors errors, Model model) throws Exception {
+
+        if (errors.hasErrors()) {
+            return "register";
+        } else {
+            try {
+                this.userService.saveUser(user);
+            } catch (UserAlreadyExistException userAlreadyExistException) {
+                model.addAttribute("userAlreadyExistError", "Un compte est déjà associé à cet email");
+                return "register";
+            }
+
+            return "redirect:/login";
+        }
+    }
+
+}
