5ffb1bea781946f53e169fc6568aebefa888b38b
joel Dumortier <joel.dumortier@gmail.com>
2023-09-29 10:40:09 +0200

    add lombok annotations

--- src/main/java/com/openclassrooms/payMyBuddy/model/Payment.java
+++ src/main/java/com/openclassrooms/payMyBuddy/model/Payment.java
@@ -1,10 +1,16 @@
 package com.openclassrooms.payMyBuddy.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Builder
 public class Payment {
 
     @Id
