ab007d54d6c49987545aa5afda0ff502c51ab2a4
joel Dumortier <joel.dumortier@gmail.com>
2023-09-29 10:13:50 +0200

    add method in account rpository to find account by user mail

--- src/main/java/com/openclassrooms/payMyBuddy/repository/AccountRepository.java
+++ src/main/java/com/openclassrooms/payMyBuddy/repository/AccountRepository.java
@@ -4,7 +4,11 @@ import com.openclassrooms.payMyBuddy.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    Optional<Account> findByUserMail(String mail);
 
 }
