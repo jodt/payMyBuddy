69ecd2d759fe696d4fe19fe9a9b86b130395d2df
joel Dumortier <joel.dumortier@gmail.com>
2023-10-06 11:31:34 +0200

    rename the package and change the amout field type to bigdecimal

--- src/main/java/com/openclassrooms/payMyBuddy/controller/dto/PaymentDTO.java
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/dto/PaymentDTO.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.payMyBuddy.Controller.dto;
+package com.openclassrooms.payMyBuddy.controller.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
@@ -17,7 +19,7 @@ public class PaymentDTO {
     private String receiverMail;
 
     @Min(value = 1)
-    private double amount;
+    private BigDecimal amount;
 
     @NotEmpty
     private String description;
