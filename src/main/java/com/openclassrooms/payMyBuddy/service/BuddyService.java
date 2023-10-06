a592eff36efa6d08e3bc7d776dd5fa14e67d44a9
joel Dumortier <joel.dumortier@gmail.com>
2023-10-06 13:52:45 +0200

    create feature to add buddy

---
+++ src/main/java/com/openclassrooms/payMyBuddy/service/BuddyService.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.payMyBuddy.service;
+
+import com.openclassrooms.payMyBuddy.controller.dto.BuddyDTO;
+import com.openclassrooms.payMyBuddy.exceptions.AlreadyBuddyExistException;
+import com.openclassrooms.payMyBuddy.model.User;
+
+import java.util.List;
+
+public interface BuddyService {
+
+    public List<BuddyDTO> getAllUsersMailAsBuddyMailsDTO();
+
+    public User addBuddy(String mail) throws AlreadyBuddyExistException;
+
+}
---
+++ src/main/java/com/openclassrooms/payMyBuddy/service/BuddyServiceImpl.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.payMyBuddy.service;
+
+import com.openclassrooms.payMyBuddy.controller.dto.BuddyDTO;
+import com.openclassrooms.payMyBuddy.controller.mapper.BuddyMapper;
+import com.openclassrooms.payMyBuddy.exceptions.AlreadyBuddyExistException;
+import com.openclassrooms.payMyBuddy.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BuddyServiceImpl implements BuddyService {
+
+    private final UserService userService;
+
+    private final BuddyMapper buddyMapper;
+
+
+    public BuddyServiceImpl(UserService userService, BuddyMapper buddyMapper) {
+        this.userService = userService;
+        this.buddyMapper = buddyMapper;
+    }
+
+    public List<BuddyDTO> getAllUsersMailAsBuddyMailsDTO() {
+        return this.userService.findAllOtherUsers().stream()
+                .map(user -> this.buddyMapper.asBuddyDTO(user))
+                .collect(Collectors.toList());
+    }
+
+    public User addBuddy(String mail) throws AlreadyBuddyExistException {
+        return this.userService.addBuddy(mail);
+    }
+
+}
