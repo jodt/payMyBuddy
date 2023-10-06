627016753549dcfd44c5a627f2c1b873209254c3
joel Dumortier <joel.dumortier@gmail.com>
2023-10-06 11:32:55 +0200

    rename package and use roundig methode for bigdecimal

--- src/main/java/com/openclassrooms/payMyBuddy/controller/mapper/AccountMapperImpl.java
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/mapper/AccountMapperImpl.java
@@ -1,10 +1,11 @@
-package com.openclassrooms.payMyBuddy.Controller.mapper;
+package com.openclassrooms.payMyBuddy.controller.mapper;
 
-import com.openclassrooms.payMyBuddy.Controller.dto.AccountDTO;
+import com.openclassrooms.payMyBuddy.controller.dto.AccountDTO;
 import com.openclassrooms.payMyBuddy.model.Account;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class AccountMapperImpl implements AccountMapper {
@@ -12,7 +13,7 @@ public class AccountMapperImpl implements AccountMapper {
     @Override
     public AccountDTO asAccountDTO(Account account) {
         return AccountDTO.builder()
-                .balance(new BigDecimal(account.getBalance()).setScale(2))
+                .balance(new BigDecimal(account.getBalance()).setScale(2, RoundingMode.HALF_DOWN))
                 .build();
     }
 
