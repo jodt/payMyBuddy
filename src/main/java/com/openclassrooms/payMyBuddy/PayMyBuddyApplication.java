732fdce58ecfe2f577c7c89d41f3a92b1adfc4ee
joel Dumortier <joel.dumortier@gmail.com>
2023-10-20 22:11:14 +0200

    add annotation to enable transactional

--- src/main/java/com/openclassrooms/payMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/payMyBuddy/PayMyBuddyApplication.java
@@ -2,8 +2,10 @@ package com.openclassrooms.payMyBuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class PayMyBuddyApplication {
 
     public static void main(String[] args) {
