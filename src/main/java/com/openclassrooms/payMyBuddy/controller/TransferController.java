7e477a823e9c9384c489e3686e03d57d3bbf2972
joel Dumortier <joel.dumortier@gmail.com>
2023-11-01 10:24:42 +0100

    create transfer page and transfer controller

---
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/TransferController.java
@@ -0,0 +1,76 @@
+package com.openclassrooms.payMyBuddy.controller;
+
+import com.openclassrooms.payMyBuddy.controller.dto.BankAccountDTO;
+import com.openclassrooms.payMyBuddy.controller.dto.TransferDTO;
+import com.openclassrooms.payMyBuddy.controller.dto.UserDTO;
+import com.openclassrooms.payMyBuddy.exceptions.InsufficientBalanceException;
+import com.openclassrooms.payMyBuddy.service.BankAccountService;
+import com.openclassrooms.payMyBuddy.service.TransferService;
+import com.openclassrooms.payMyBuddy.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TransferController {
+
+    private final TransferService transferService;
+
+    private final UserService userService;
+
+    private final BankAccountService bankAccountService;
+
+
+    public TransferController(TransferService transferService, UserService userService, BankAccountService bankAccountService) {
+        this.transferService = transferService;
+        this.userService = userService;
+        this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping("/transfer")
+    public String showTransferPage(Model model) {
+        model.addAttribute("debitTransfer", new TransferDTO());
+        model.addAttribute("creditTransfer", new TransferDTO());
+        return ("transfer");
+    }
+
+
+    @PostMapping("/transfer/credit")
+    public String creditAccount(@Valid @ModelAttribute("creditTransfer") TransferDTO transferDTO, Errors errors, Model model) {
+        UserDTO loggedUser = this.userService.getLoggedUserDTO();
+        BankAccountDTO userBankAccountDTO = this.bankAccountService.findBankAccountDTOByUserMail(loggedUser.getMail());
+
+        if (errors.hasErrors()) {
+            model.addAttribute("debitTransfer", new TransferDTO());
+            return ("transfer");
+        }
+
+        this.transferService.makeCreditTransfer(transferDTO, loggedUser.getMail());
+
+        return ("redirect:../home?creditsuccess");
+    }
+
+    @PostMapping("/transfer/debit")
+    public String debitAccount(@Valid @ModelAttribute("debitTransfer") TransferDTO transferDTO, Errors errors, Model model) {
+        UserDTO loggedUser = this.userService.getLoggedUserDTO();
+        BankAccountDTO userBankAccountDTO = this.bankAccountService.findBankAccountDTOByUserMail(loggedUser.getMail());
+
+        if (errors.hasErrors()) {
+            if (errors.hasErrors()) {
+                model.addAttribute("creditTransfer", new TransferDTO());
+                return ("transfer");
+            }
+        }
+        try {
+            this.transferService.makeDebitTransfer(transferDTO, loggedUser.getMail());
+        } catch (InsufficientBalanceException e) {
+            return ("redirect:../home?debiterror");
+        }
+        return ("redirect:../home?debitsuccess");
+    }
+
+}
