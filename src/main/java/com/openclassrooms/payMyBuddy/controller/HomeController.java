b43a130c417e44230280eccd5b2eea00a49aa47b
joel Dumortier <joel.dumortier@gmail.com>
2023-11-01 10:28:53 +0100

    add the bankAccount attribute

--- src/main/java/com/openclassrooms/payMyBuddy/controller/HomeController.java
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/HomeController.java
@@ -1,9 +1,10 @@
 package com.openclassrooms.payMyBuddy.controller;
 
 import com.openclassrooms.payMyBuddy.controller.dto.AccountDTO;
+import com.openclassrooms.payMyBuddy.controller.dto.BankAccountDTO;
 import com.openclassrooms.payMyBuddy.controller.mapper.AccountMapper;
-import com.openclassrooms.payMyBuddy.model.Account;
 import com.openclassrooms.payMyBuddy.service.AccountService;
+import com.openclassrooms.payMyBuddy.service.BankAccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -20,15 +20,20 @@ public class HomeController {
 
     private final AccountService accountService;
 
-    public HomeController(AccountService accountService, AccountMapper accountMapper) {
+    private final BankAccountService bankAccountService;
+
+    public HomeController(AccountService accountService, AccountMapper accountMapper, BankAccountService bankAccountService) {
         this.accountService = accountService;
+        this.bankAccountService = bankAccountService;
     }
 
     @GetMapping("/home")
     public String showHomePage(Principal principal, Model model) {
         log.info("GET /Home called");
         AccountDTO userAccount = this.accountService.findAccountDtoByUserMail(principal.getName());
+        BankAccountDTO userBankAccountDto = this.bankAccountService.findBankAccountDTOByUserMail(principal.getName());
         model.addAttribute("date", LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        model.addAttribute("bankAccount", userBankAccountDto);
         model.addAttribute("account", userAccount);
         log.info("Home page displayed");
         return ("home");
