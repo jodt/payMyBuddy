2f31a4114abeec66706c38584b686566d32f3c61
joel Dumortier <joel.dumortier@gmail.com>
2023-11-09 08:55:39 +0100

    update tests after removing controller transfer functionality

--- src/test/java/com/openclassrooms/payMyBuddy/controller/ProfilControllerTest.java
+++ src/test/java/com/openclassrooms/payMyBuddy/controller/ProfilControllerTest.java
@@ -11,11 +11,9 @@ import com.openclassrooms.payMyBuddy.model.User;
 import com.openclassrooms.payMyBuddy.service.BankAccountService;
 import com.openclassrooms.payMyBuddy.service.TransferService;
 import com.openclassrooms.payMyBuddy.service.UserService;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.stubbing.BaseStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,17 +22,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ProfilController.class)
@@ -101,15 +95,6 @@ class ProfilControllerTest {
                 .iban("FR12121212")
                 .build();
 
-        transferDTO = TransferDTO.builder()
-                .amount(new BigDecimal("100"))
-                .build();
-
-        transfer = Transfer.builder()
-                .account(account)
-                .bankAccount(bankAccount)
-                .amount(100)
-                .build();
 
     }
 
@@ -126,8 +111,7 @@ class ProfilControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("profil"))
                 .andExpect(model().attribute("userBankAccount", hasProperty("iban", equalTo("FR12121212"))))
-                .andExpect(model().attribute("loggedUser", userDTO))
-                .andExpect(model().attributeExists("debitTransfer", "creditTransfer"));
+                .andExpect(model().attribute("loggedUser", userDTO));
 
         verify(this.userService).getLoggedUserDTO();
         verify(this.bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
@@ -159,110 +143,6 @@ class ProfilControllerTest {
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("profil?ibanError"));
-    }
-
-    @Test
-    @WithMockUser("john@test.com")
-    @DisplayName("Should credit the  account")
-    void shouldCreditAccount() throws Exception {
-
-        when(this.userService.getLoggedUserDTO()).thenReturn(userDTO);
-        when(this.bankAccountService.findBankAccountDTOByUserMail(userDTO.getMail())).thenReturn(bankAccountDTO);
-        when(this.transferService.makeCreditTransfer(transferDTO, userDTO.getMail())).thenReturn(transfer);
-
-        mockMvc.perform(post("/profil/credit")
-                        .with(csrf())
-                        .flashAttr("creditTransfer", transferDTO))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("../home?creditsuccess"));
-
-        verify(userService).getLoggedUserDTO();
-        verify(bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
-        verify(transferService).makeCreditTransfer(transferDTO, userDTO.getMail());
-    }
-
-    @Test
-    @WithMockUser("john@test.com")
-    @DisplayName("Should not credit the account -> credit transfer has error field")
-    void ShouldNotCreditAccount() throws Exception {
-
-
-        when(this.userService.getLoggedUserDTO()).thenReturn(userDTO);
-        when(this.bankAccountService.findBankAccountDTOByUserMail(userDTO.getMail())).thenReturn(bankAccountDTO);
-
-        mockMvc.perform(post("/profil/credit")
-                        .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("profil"))
-                .andExpect(model().attribute("userBankAccount", bankAccountDTO))
-                .andExpect(model().attributeExists("debitTransfer"))
-                .andExpect(model().attributeHasFieldErrors("creditTransfer", "amount"));
-
-        verify(userService).getLoggedUserDTO();
-        verify(bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
-        verify(transferService, never()).makeCreditTransfer(transferDTO, userDTO.getMail());
-    }
-
-    @Test
-    @WithMockUser("john@test.com")
-    @DisplayName("Should debit the account")
-    void debitAccount() throws Exception {
-
-        when(this.userService.getLoggedUserDTO()).thenReturn(userDTO);
-        when(this.bankAccountService.findBankAccountDTOByUserMail(userDTO.getMail())).thenReturn(bankAccountDTO);
-        when(this.transferService.makeDebitTransfer(transferDTO, userDTO.getMail())).thenReturn(transfer);
-
-        mockMvc.perform(post("/profil/debit")
-                        .with(csrf())
-                        .flashAttr("debitTransfer", transferDTO))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("../home?debitsuccess"));
-
-        verify(userService).getLoggedUserDTO();
-        verify(bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
-        verify(transferService).makeDebitTransfer(transferDTO, userDTO.getMail());
-    }
-
-    @Test
-    @WithMockUser("john@test.com")
-    @DisplayName("Should not debit the account -> debit transfer has error field")
-    void ShouldNotDebitAccount() throws Exception {
-
-
-        when(this.userService.getLoggedUserDTO()).thenReturn(userDTO);
-        when(this.bankAccountService.findBankAccountDTOByUserMail(userDTO.getMail())).thenReturn(bankAccountDTO);
-
-        mockMvc.perform(post("/profil/debit")
-                        .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("profil"))
-                .andExpect(model().attribute("userBankAccount", bankAccountDTO))
-                .andExpect(model().attributeExists("creditTransfer"))
-                .andExpect(model().attributeHasFieldErrors("debitTransfer", "amount"));
-
-        verify(userService).getLoggedUserDTO();
-        verify(bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
-        verify(transferService, never()).makeCreditTransfer(transferDTO, userDTO.getMail());
-    }
-
-    @Test
-    @WithMockUser("john@test.com")
-    @DisplayName("Should not debit the account -> InsufficientBalanceException")
-    void ShouldNotDebitAccountInsufficientBalanceException() throws Exception {
-
-
-        when(this.userService.getLoggedUserDTO()).thenReturn(userDTO);
-        when(this.bankAccountService.findBankAccountDTOByUserMail(userDTO.getMail())).thenReturn(bankAccountDTO);
-        when(this.transferService.makeDebitTransfer(transferDTO, userDTO.getMail())).thenThrow(InsufficientBalanceException.class);
-
-        mockMvc.perform(post("/profil/debit")
-                        .with(csrf())
-                        .flashAttr("debitTransfer", transferDTO))
-                .andExpect(redirectedUrl("../home?debiterror"));
-
-        verify(userService).getLoggedUserDTO();
-        verify(bankAccountService).findBankAccountDTOByUserMail(userDTO.getMail());
-        verify(transferService).makeDebitTransfer(transferDTO, userDTO.getMail());
     }
 
 }
