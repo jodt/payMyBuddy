6773bafe1323dacfa031325ed05506e0adb6e565
joel Dumortier <joel.dumortier@gmail.com>
2023-11-09 17:11:35 +0100

    create UserbankAccountException

---
+++ src/main/java/com/openclassrooms/payMyBuddy/exceptions/UserBankAccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.payMyBuddy.exceptions;
+
+public class UserBankAccountNotFoundException extends Exception {
+
+    public UserBankAccountNotFoundException() {
+        super();
+    }
+
+}
