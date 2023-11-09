3a0185903e0b40dfd9f5e4fe2895532d29e41c3c
joel Dumortier <joel.dumortier@gmail.com>
2023-11-09 16:18:48 +0100

    add logs

--- src/main/java/com/openclassrooms/payMyBuddy/controller/TransferController.java
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/TransferController.java
@@ -9,13 +9,16 @@ import com.openclassrooms.payMyBuddy.service.BankAccountService;
 import com.openclassrooms.payMyBuddy.service.TransferService;
 import com.openclassrooms.payMyBuddy.service.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class TransferController {
 
@@ -31,17 +34,21 @@ public class TransferController {
 
     @GetMapping("/transfer")
     public String showTransferPage(Model model) {
+        log.info("GET /transfer called");
         model.addAttribute("debitTransfer", new TransferDTO());
         model.addAttribute("creditTransfer", new TransferDTO());
+        log.info("Transfer page displayed");
         return ("transfer");
     }
 
 
     @PostMapping("/transfer/credit")
     public String creditAccount(@Valid @ModelAttribute("creditTransfer") TransferDTO transferDTO, Errors errors, Model model) throws UserNotFoundException {
+        log.info("POST /transfer/credit called -> start of the process to make a credit transfer");
         UserDTO loggedUser = this.userService.getLoggedUserDTO();
 
         if (errors.hasErrors()) {
+            log.info("Errors in form validation on field {}", errors.getFieldError().getField());
             model.addAttribute("debitTransfer", new TransferDTO());
             return ("transfer");
         }
@@ -53,9 +60,11 @@ public class TransferController {
 
     @PostMapping("/transfer/debit")
     public String debitAccount(@Valid @ModelAttribute("debitTransfer") TransferDTO transferDTO, Errors errors, Model model) throws UserNotFoundException {
+        log.info("POST /transfer/debit called -> start of the process to make a debit transfer");
         UserDTO loggedUser = this.userService.getLoggedUserDTO();
 
         if (errors.hasErrors()) {
+            log.info("Errors in form validation on field {}", errors.getFieldError().getField());
             model.addAttribute("creditTransfer", new TransferDTO());
             return ("transfer");
         }
@@ -63,6 +72,7 @@ public class TransferController {
         try {
             this.transferService.makeDebitTransfer(transferDTO, loggedUser.getMail());
         } catch (InsufficientBalanceException e) {
+            log.info("insufficient balance to make this transfer");
             return ("redirect:../home?debiterror");
         }
         return ("redirect:../home?debitsuccess");
